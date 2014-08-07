32424b6b53a46340132de34fbe53b563b30a1f29
桜井 克朗 <k_sakurai@yumemi.co.jp>
2014-08-07 10:44:51 +0900

    Initial Commit

---
+++ sample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  sample
+//
+//  Created by 桜井 克朗 on 2014/08/07.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
